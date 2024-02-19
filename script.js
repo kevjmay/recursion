6a1b012a55e4522faa5670dc7d2e3c16e549433f
kevjmay <kevinjohnmyburgh@icloud.com>
2024-02-19 20:46:28 +0000

    assignment 2 done

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Fibonacci Numbers in Array
 let fibArr = [];
 
 function fibNumbers(n) {
@@ -15,6 +16,8 @@ function fibNumbers(n) {
 
 fibNumbers(8)
 
+//Fibonacci Numbers with Recursion
+
 function fibRec(n) {
     if (n < 2) {
         return n
@@ -24,3 +27,44 @@ function fibRec(n) {
 };
 
 console.log(fibRec(8))
+
+//Merg Sort
+function mergeSort(arr) {
+    if (arr.length <= 1) {
+        return arr
+    } 
+
+    const mid = Math.floor(arr.length / 2);
+    const left = arr.slice(0, mid);
+    const right = arr.slice(mid);
+
+    const sortedLeft = mergeSort(left);
+    const sortedRight = mergeSort(right);
+
+    return merge(sortedLeft, sortedRight);
+}
+
+function merge(left, right) {
+    let result = [];
+    let leftIndex = 0;
+    let rightIndex = 0;
+
+    while (leftIndex < left.length && rightIndex < right.length) {
+        if (left[leftIndex] < right[rightIndex]) {
+            result.push(left[leftIndex]);
+            leftIndex++;
+        } else {
+            result.push(right[rightIndex]);
+            rightIndex++;
+        }
+    }
+
+    return result.concat(left.slice(leftIndex)).concat(right.slice(rightIndex));
+}
+
+const arr = [3, 2, 1, 13, 8, 5, 0, 1];
+const arr2 = [105, 79, 100, 110]
+const arrSorted = mergeSort(arr)
+const arr2Sorted = mergeSort(arr2)
+console.log(arrSorted)
+console.log(arr2Sorted)
